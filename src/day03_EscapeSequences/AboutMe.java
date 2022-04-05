6c285c25fbd1d1f0858b49ac81b925c94dadba3b
Berzeyefe <erolaksel1@gmail.com>
2022-04-05 23:58:04 +0300

    I have been added newly

---
+++ src/day03_EscapeSequences/AboutMe.java
@@ -0,0 +1,26 @@
+package day03_EscapeSequences;
+
+public class AboutMe<Crea> {
+
+    public static void main(String[] args) {
+
+        System.out.println("\tMy favorite music is \"Pop\"musics. \n\tMy favorite book is \"Java\"");
+
+
+     }
+
+     /*
+           Create a class called About me
+favorite music
+favorite book
+favorite quote
+favorite Tv show
+      */
+
+
+
+
+
+
+
+}
