12edaae867553d021a3ab69734411d8366aabadd
Berzeyefe <erolaksel1@gmail.com>
2022-04-06 01:39:48 +0300

    newly added

---
+++ src/day03_EscapeSequences/EscapeSequences.java
@@ -0,0 +1,18 @@
+package day03_EscapeSequences;
+
+public class EscapeSequences {
+
+    public static void main(String[] args) {
+
+        System.out.println("Java \nPyton \nC#");
+        System.out.println("-----------------------------");
+        System.out.println("Hello Cydeo \nHoware you all today? \nİt is nice to see you all ");
+        System.out.println("-----------------------------");
+        System.out.println("\tJava is Cool Programming Language");
+        System.out.println("-----------------------------");
+        System.out.println("/ \\"); // in order to print one backward slash we need to give two backward slashes
+                System.out.println("-----------------------------");
+        System.out.println("My favorite tv show is \"Game Of Thrones\"");
+
+    }
+}
