9d855a67c70098772c5b349e7235d61a5f51b451
Berzeyefe <erolaksel1@gmail.com>
2022-04-06 00:02:54 +0300

    I have been added newly

---
+++ src/day07_Operators/ArithmeticOperators.java
@@ -0,0 +1,36 @@
+package day07_Operators;
+
+public class ArithmeticOperators {
+
+    public static void main(String[] args) {
+
+        int num1 = 10;
+        int num2 = 3;
+
+        int division = num1 / num2; // division variable contains the result of num1 divided by num2
+        int remainder = num1 % num2; // remainder variable contains the remainder of num1 divided by number2
+
+        // 10 divide 3 is equal to 3 with a remainder of 1
+
+        System.out.println(num1 + " divided by " + num2 + " is equal to " + division + " with a remainder of " + remainder );
+
+        int num3 = 9;
+        int num4 = 3;
+
+        // 9 divide 3 is equal to 3 with a remainder of 0
+
+        System.out.println(num1 + " divided by " + num2 + " is equal to " + division + " with a remainder of " + remainder );
+
+        // whats the remainder of 25 divided 4
+
+        System.out.println( 25 % 4 );
+
+        // whats the remainder of 25 divided 5
+
+        System.out.println( 25 % 5 );
+
+
+    }
+
+
+}
