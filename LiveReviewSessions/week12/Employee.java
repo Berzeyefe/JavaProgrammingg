a6ffe4105508ba20b007fc4d72bf23f111cef499
Berzeyefe <erolaksel1@gmail.com>
2022-04-06 01:39:16 +0300

    newly added

---
+++ LiveReviewSessions/week12/Employee.java
@@ -0,0 +1,32 @@
+package week12;
+
+public class Employee {
+
+          public String name, JobTitle;
+          public int ID;
+          public double salary;
+
+          public Employee(){
+
+
+
+          }
+
+
+    public Employee(String s) {
+    }
+}
+
+
+
+
+/*Employee:
+        attribute
+        name, jobTitle, ID, salary
+
+        add a constructor to initialize all the fields
+
+        toString()
+
+
+ */
---
+++ src/day08_IfStatements/EligibleToVote.java
@@ -0,0 +1,24 @@
+package day08_IfStatements;
+
+public class EligibleToVote {
+
+    public static void main(String[] args) {
+
+        String name = "Josh";
+        int age = 38;
+        String citizen = "USA";
+
+        boolean isEligible = age >= 21 && citizen == "USA";// true
+
+        // eligible
+        if(isEligible){ // true
+            System.out.println("Eligible");
+        }
+
+        //not eligible
+        if(!isEligible){// !true >>>>not true>>>>  false
+            System.out.println(" notEligible ");
+        }
+
+    }
+}
