1f51473f1210f712a749a2d1c4c06a38d5187ace
Berzeyefe <erolaksel1@gmail.com>
2022-04-27 13:21:07 +0300

    practice

--- AvengersHoursPractice/week05/Locators.java
+++ AvengersHoursPractice/week05/Locators.java
@@ -1,33 +1,23 @@
 package week05;
 
 public class Locators {
-    public Locators(String locator) {
-    }
-
-    public class locators {
-        String locator;
-
-        public void Locators(String locator) {
-            this.locator = locator;
-        }
-    }
-
-    class ID extends Locators {
-        public ID(String locator) {
-            super(locator);
-        }
-    }
-
-    class Name extends Locators {
-        public Name(String locator) {
-            super(locator);
-        }
-    }
-
-    class TagName extends Locators {
-        public TagName(String locator) {
-            super(locator);
-        }
+    String locator;
+    public Locators(String locator){
+        this.locator=locator;
     }
 }
-
+class ID extends Locators{
+    public ID(String locator) {
+        super(locator);
+    }
+}
+class Name extends Locators{
+    public Name(String locator) {
+        super(locator);
+    }
+}
+class TagName extends Locators{
+    public TagName(String locator) {
+        super(locator);
+    }
+}
