5cd0df063749161f69ee55966bcd451095bf14c1
Berzeyefe <erolaksel1@gmail.com>
2022-05-06 15:58:47 +0300

    practice

---
+++ src/Practice/Practice.java
@@ -0,0 +1,15 @@
+package Practice;
+
+public class Practice {
+
+    public class Employee {
+
+        //line 2
+
+        public int passportNo;
+
+
+
+
+    }
+}
--- src/day28_ArrayList/ArrayListMethods1.java
+++ src/day28_ArrayList/ArrayListMethods1.java
@@ -1,11 +1,13 @@
 package day28_ArrayList;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class ArrayListMethods1 {
 
     public static void main(String[] args) {
-        ArrayList<Integer>  numbers = new ArrayList<>();
+        ArrayList<Integer> numbers = new ArrayList<>();
         numbers.add(10); //0
         numbers.add(20); //1
         numbers.add(30); //3
@@ -19,7 +21,7 @@ public class ArrayListMethods1 {
         System.out.println(numbers);
 
 
-        System.out.println( numbers.size() );
+        System.out.println(numbers.size());
 
         int lastIndex = numbers.size() - 1;
 
@@ -32,7 +34,7 @@ public class ArrayListMethods1 {
         System.out.println("------------------------------------");
 
         for (int i = 0; i < numbers.size(); i++) {
-            System.out.println( numbers.get(i) );
+            System.out.println(numbers.get(i));
         }
 
         System.out.println("----------------------------------");
@@ -78,7 +80,7 @@ public class ArrayListMethods1 {
 
         System.out.println(employees);
 
-        employees.remove( employees.size() -1 );
+        employees.remove(employees.size() - 1);
 
         System.out.println(employees);
 
@@ -93,7 +95,34 @@ public class ArrayListMethods1 {
 
         System.out.println("r1 = " + r1);
         System.out.println("r2 = " + r2);
+        System.out.println(".............................");
 
-    }
-}
+
+
+                ///---> Java ArrayList Kullanımı Örneği <---\\\
+                List<Integer> myRandomList = new ArrayList<>();
+                Random random = new Random();
+                int value, total = 0, cift = 0, tek = 0;
+                System.out.print("----------------------\n< Random ArrayList Oluşturma >\n----------------------\n-> Oluşturulan Dizi Elemanları: ");
+                for (int i = 0; i < 10; i++) {
+                    value = random.nextInt(1000) + 1;
+                    myRandomList.add(value);
+                    total += myRandomList.get(i);
+                    if (myRandomList.get(i) % 2 == 0)
+                        cift += 1;
+                    else
+                        tek += 1;
+                }
+                    System.out.print("[" + myRandomList.get('i') + "] ");
+
+                   // Bir ip ile lastik bir bant düşünelim. Lastik bantın yapısı gergin olmadığında küçük olmasına rağmen, bantı çektikçe uzamaktadır.
+                    //ArrayList’de lastik bant gibi içine değer aktarıldığında büyümekte içerisinden değer çıkarıldığında küçülmektedir.
+                    // Diziyi ip olarak düşünürsek ipin uzunluğu sabit olduğu için her hangi bir büyüme ve küçülme söz konusu değildir.
+
+                }
+
+
+            }
+
+
 
