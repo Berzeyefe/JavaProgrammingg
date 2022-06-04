8d5d0bb80b9c418f8a2fab3a3196f4c5e23b8498
Berzeyefe <erolaksel1@gmail.com>
2022-06-04 18:30:32 +0300

    week20 review class

---
+++ LiveReviewSessions/week20/MapExampleWithClassObject.java
@@ -0,0 +1,12 @@
+package week20;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class MapExampleWithClassObject {
+
+    public static void main(String[] args) {
+        Map<String,Object> cydeo= new LinkedHashMap<>();
+
+    }
+}
